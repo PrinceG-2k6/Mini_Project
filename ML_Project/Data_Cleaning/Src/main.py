a7d22b39072c6fc9f7414d090969114c1a2e8a02
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-16 15:38:55 +0530

    .

--- ML_Project/Data_Cleaning/Src/main.py
+++ ML_Project/Data_Cleaning/Src/main.py
@@ -9,7 +9,7 @@ from visualization import visualize
 
 
 path = input("Enter CSV file path \n(Try 'Titanic' OR Enter Path if you have CSV) : ")
-
+df = None
 if path.lower() == "titanic":
     path = "./ML_Project/Data_Cleaning/Src/Titanic.csv"
     df = pd.read_csv(path)
@@ -20,8 +20,10 @@ else:
 print("\nFile uploaded successfully!")
 
 
+  
 def menu():
     print("\n===== DATA CLEANING MENU =====")
+    print("0. Show All Data")
     print("1. Show dataset info")
     print("2. Handle missing values")
     print("3. Remove duplicates")
@@ -41,7 +43,10 @@ while True:
     menu()
     choice = int(input("Enter choice: "))
 
-    if choice == 1:
+    if choice == 0:
+        pd.set_option('display.max_rows', None)
+        print(df)
+    elif choice == 1:
         show_info(df)
     elif choice == 2:
         handle_missing(df)
@@ -58,6 +63,6 @@ while True:
         break
 
 
-# pd.set_option('display.max_rows', None)
-# print(df)
+pd.set_option('display.max_rows', None)
+print(df)
   
