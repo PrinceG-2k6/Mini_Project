af7fb4e293b856ed68612326e90a4792354c0e17
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-19 19:21:41 +0530

    .

--- ML_Project/Data_Cleaning/Src/main.py
+++ ML_Project/Data_Cleaning/Src/main.py
@@ -25,19 +25,60 @@ def menu():
     print("\n===== DATA CLEANING MENU =====")
     print("0. Show All Data")
     print("1. Show dataset info")
-    print("2. Handle missing values")
-    print("3. Remove duplicates")
-    print("4. Handle outliers")
-    print("5. Data visualization")
-    print("6. Save cleaned data")
-    print("7. Exit")
+    print("2. Search Data")
+    print("3. Handle missing values")
+    print("4. Remove duplicates")
+    print("5. Handle outliers")
+    print("6. Data visualization")
+    print("7. Save cleaned data")
+    print("8. Exit")
 
 
 def show_info(df):
     print("\nDataset Info : \n")
     print(df.info())
     print("\nMissing Values: \n")
-    print(df.isna().sum()[df.isna().sum() > 0])
+    print(df.isna().sum()[df.isna().sum() > 0])\
+    
+def search_info(df):
+    columns = df.columns.tolist()
+
+    # Show columns with numbers
+    print("\nAvailable Columns:")
+    for i, col in enumerate(columns, 1):
+        print(f"{i}. {col}")
+
+    try:
+        col_choice = int(input("Select column number: "))
+
+        if col_choice < 1 or col_choice > len(columns):
+            raise ValueError
+
+        column_name = columns[col_choice - 1]
+
+        value = input(f"Enter value to search in '{column_name}': ")
+
+        # Search logic
+        if df[column_name].dtype == object:
+            result = df[df[column_name].astype(str)
+                        .str.contains(value, case=False, na=False)]
+        else:
+            try:
+                value = float(value)
+                result = df[df[column_name] == value]
+            except:
+                print("❌ Invalid numeric value!")
+                
+
+        if result.empty:
+            print("❌ No matching records found!")
+        else:
+            print("\nMatching Records:")
+            print(result)
+
+    except ValueError:
+        print("❌ Invalid column selection!")
+
 
 while True:
     menu()
@@ -47,19 +88,21 @@ while True:
         pd.set_option('display.max_rows', None)
         print(df)
     elif choice == '1':
-        show_info(df)
+        search_info(df)
     elif choice == '2':
-        handle_missing(df)
+        show_info(df)
     elif choice == '3':
-        remove_duplicates(df)
+        handle_missing(df)
     elif choice == '4':
+        remove_duplicates(df)
+    elif choice == '5':
         col = input("Enter column for outliers: ")
         df = handle_outliers(df, col)
-    elif choice == '5':
-        visualize(df)
     elif choice == '6':
-        df.to_csv("data/cleaned.csv", index=False)
+        visualize(df)
     elif choice == '7':
+        df.to_csv("data/cleaned.csv", index=False)
+    elif choice == '8':
         break
     else:
         print("\n=================================")
